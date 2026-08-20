1e9d0b5de76531f9c5a4d58720b275b57789ac12
yovoweqif674-cyber <yovoweqif674@gmail.com>
2026-08-20 15:41:41 +0300

    fix: repin LF-normalized Phase A helper

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 readonly BOOTSTRAP_REPOSITORY='yovoweqif674-cyber/pokerops-bootstrap'
-readonly HELPER_COMMIT='bcf9ff0436561c5f139df7086378cd65ec7e9b09'
+readonly HELPER_COMMIT='0117d6102efc5abbfed053881eb4bb5ab99eff34'
 readonly APPLICATION_COMMIT='f2b7d46b49da90c0e964ef9b0e65df1900a533e1'
 readonly PHASE_A_HELPER_SHA256='d0931cac0526a2e687880e2f05ba238156f4e635bb979f06e89c4d9cae22627e'
 
@@ -23,5 +23,4 @@ chmod 0700 "$phase_a_helper"
 bash "$phase_a_helper" "$APPLICATION_COMMIT"
 
 printf '%s\n'   'TOURNAMENT PLANNER WRAPPER SUCCESS'   "application_release=$APPLICATION_COMMIT"   'phase=planner-maintenance'   'scheduler_enabled=false'   'job_worker_enabled=false'   'frontend_deployed=false'   'cleanup_applied=false'   'canary_activated=false'
-
 
